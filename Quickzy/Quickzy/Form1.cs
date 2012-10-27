6f82d2dbb1a81731dcf7ed7a763f6ef9023e777e
kostya9@gmail.com <kostya9@gmail.com@7f1c5c33-4fbd-76e4-5a14-5bb6ed2d70da>
2012-10-27 15:46:24 +0000

    Images read from folder

--- Quickzy/Quickzy/Form1.cs
+++ Quickzy/Quickzy/Form1.cs
@@ -6,14 +6,69 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Quickzy
 {
     public partial class Form1 : Form
     {
+        private readonly List<Item> items = new List<Item>();
+        private int[] selectedIndexes = new int[3];
+        private int correctItemIndex;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadItems(@"..\..\Images\Машины");
+            SelectCandidates();
+            ShowImage();
+            UpdateRadioButtons();
+        }
+
+        private void ShowImage()
+        {
+            pbImage.ImageLocation = items[correctItemIndex].FileName;
+        }
+
+        private void UpdateRadioButtons()
+        {
+            llAnswer1.Text = items[selectedIndexes[0]].Text;
+            llAnswer2.Text = items[selectedIndexes[1]].Text;
+            llAnswer3.Text = items[selectedIndexes[2]].Text;
+        }
+
+        private void SelectCandidates()
+        {
+            Random random = new Random();
+            for (int i = 0; i < selectedIndexes.Length; i++)
+            {
+                selectedIndexes[i] = random.Next(0, items.Count);
+            }
+            int correctIndexInSelectedIndexes = random.Next(0, selectedIndexes.Length);
+            correctItemIndex = selectedIndexes[correctIndexInSelectedIndexes];
+        }
+
+        private void LoadItems(string imagePath)
+        {
+            foreach (string fileName in Directory.EnumerateFiles(imagePath))
+            {
+                Item item = new Item
+                {
+                    FileName = imagePath + @"\" + fileName,
+                    Text = GetTextFromFileName(fileName)
+                };
+                items.Add(item);
+            }
+        }
+
+        private string GetTextFromFileName(string fileName)
+        {
+            return Path.GetFileNameWithoutExtension(fileName);
         }
     }
 }
