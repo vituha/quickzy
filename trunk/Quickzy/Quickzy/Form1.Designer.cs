96b22b816131daefefe0b40d08e9fe454213466d
kostya9@gmail.com <kostya9@gmail.com@7f1c5c33-4fbd-76e4-5a14-5bb6ed2d70da>
2012-10-27 15:46:24 +0000

    Images read from folder

--- trunk/Quickzy/Quickzy/Form1.Designer.cs
+++ trunk/Quickzy/Quickzy/Form1.Designer.cs
@@ -30,21 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.rbAnswer1 = new System.Windows.Forms.RadioButton();
-            this.rbAnswer2 = new System.Windows.Forms.RadioButton();
-            this.rbAnswer3 = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblCorrectAnswers = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblTotalAnswers = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.llAnswer1 = new System.Windows.Forms.LinkLabel();
+            this.llAnswer2 = new System.Windows.Forms.LinkLabel();
+            this.llAnswer3 = new System.Windows.Forms.LinkLabel();
             this.statusStrip1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -59,88 +59,6 @@
             this.statusStrip1.Size = new System.Drawing.Size(857, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
-            // 
-            // panel2
-            // 
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.rbAnswer3);
-            this.panel2.Controls.Add(this.rbAnswer2);
-            this.panel2.Controls.Add(this.rbAnswer1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(657, 0);
-            this.panel2.MinimumSize = new System.Drawing.Size(200, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 440);
-            this.panel2.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.MinimumSize = new System.Drawing.Size(500, 300);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(657, 440);
-            this.panel1.TabIndex = 3;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.ImageLocation = "http://cdn4.techpp.com/wp-content/uploads/2012/07/tv.jpg";
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.MinimumSize = new System.Drawing.Size(500, 300);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(657, 440);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // rbAnswer1
-            // 
-            this.rbAnswer1.AutoSize = true;
-            this.rbAnswer1.Location = new System.Drawing.Point(25, 73);
-            this.rbAnswer1.Name = "rbAnswer1";
-            this.rbAnswer1.Size = new System.Drawing.Size(39, 17);
-            this.rbAnswer1.TabIndex = 0;
-            this.rbAnswer1.TabStop = true;
-            this.rbAnswer1.Text = "TV";
-            this.rbAnswer1.UseVisualStyleBackColor = true;
-            // 
-            // rbAnswer2
-            // 
-            this.rbAnswer2.AutoSize = true;
-            this.rbAnswer2.Location = new System.Drawing.Point(25, 119);
-            this.rbAnswer2.Name = "rbAnswer2";
-            this.rbAnswer2.Size = new System.Drawing.Size(55, 17);
-            this.rbAnswer2.TabIndex = 1;
-            this.rbAnswer2.TabStop = true;
-            this.rbAnswer2.Text = "Printer";
-            this.rbAnswer2.UseVisualStyleBackColor = true;
-            // 
-            // rbAnswer3
-            // 
-            this.rbAnswer3.AutoSize = true;
-            this.rbAnswer3.Location = new System.Drawing.Point(25, 166);
-            this.rbAnswer3.Name = "rbAnswer3";
-            this.rbAnswer3.Size = new System.Drawing.Size(70, 17);
-            this.rbAnswer3.TabIndex = 2;
-            this.rbAnswer3.TabStop = true;
-            this.rbAnswer3.Text = "Computer";
-            this.rbAnswer3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(22, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Choose correct answer:";
             // 
             // toolStripStatusLabel1
             // 
@@ -167,6 +85,85 @@
             this.lblTotalAnswers.Size = new System.Drawing.Size(13, 17);
             this.lblTotalAnswers.Text = "1";
             // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.llAnswer3);
+            this.panel2.Controls.Add(this.llAnswer2);
+            this.panel2.Controls.Add(this.llAnswer1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(657, 0);
+            this.panel2.MinimumSize = new System.Drawing.Size(200, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 440);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose correct answer:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pbImage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(500, 300);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(657, 440);
+            this.panel1.TabIndex = 3;
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbImage.ImageLocation = "http://cdn4.techpp.com/wp-content/uploads/2012/07/tv.jpg";
+            this.pbImage.InitialImage = null;
+            this.pbImage.Location = new System.Drawing.Point(0, 0);
+            this.pbImage.MinimumSize = new System.Drawing.Size(500, 300);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(657, 440);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // llAnswer1
+            // 
+            this.llAnswer1.AutoSize = true;
+            this.llAnswer1.Location = new System.Drawing.Point(27, 111);
+            this.llAnswer1.Name = "llAnswer1";
+            this.llAnswer1.Size = new System.Drawing.Size(21, 13);
+            this.llAnswer1.TabIndex = 4;
+            this.llAnswer1.TabStop = true;
+            this.llAnswer1.Text = "TV";
+            // 
+            // llAnswer2
+            // 
+            this.llAnswer2.AutoSize = true;
+            this.llAnswer2.Location = new System.Drawing.Point(27, 158);
+            this.llAnswer2.Name = "llAnswer2";
+            this.llAnswer2.Size = new System.Drawing.Size(52, 13);
+            this.llAnswer2.TabIndex = 5;
+            this.llAnswer2.TabStop = true;
+            this.llAnswer2.Text = "Computer";
+            // 
+            // llAnswer3
+            // 
+            this.llAnswer3.AutoSize = true;
+            this.llAnswer3.Location = new System.Drawing.Point(27, 205);
+            this.llAnswer3.Name = "llAnswer3";
+            this.llAnswer3.Size = new System.Drawing.Size(37, 13);
+            this.llAnswer3.TabIndex = 6;
+            this.llAnswer3.TabStop = true;
+            this.llAnswer3.Text = "Printer";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -180,12 +177,13 @@
             this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "Quickzy ;)";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,15 +194,15 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbImage;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton rbAnswer3;
-        private System.Windows.Forms.RadioButton rbAnswer2;
-        private System.Windows.Forms.RadioButton rbAnswer1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel lblCorrectAnswers;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel lblTotalAnswers;
+        private System.Windows.Forms.LinkLabel llAnswer3;
+        private System.Windows.Forms.LinkLabel llAnswer2;
+        private System.Windows.Forms.LinkLabel llAnswer1;
 
     }
 }
---
+++ trunk/Quickzy/Quickzy/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quickzy
+{
+    class Item
+    {
+        public string Text;
+        public string FileName;
+    }
+}
