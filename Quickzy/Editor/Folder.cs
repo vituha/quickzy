dec79613b138798f3ebcd21ca8d6eea9a2d387b5
kostya9@gmail.com <kostya9@gmail.com@7f1c5c33-4fbd-76e4-5a14-5bb6ed2d70da>
2013-01-07 07:31:45 +0000

    Added Editor.

---
+++ Quickzy/Editor/Folder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Editor
+{
+    class Folder
+    {
+        public string FolderPath { get; set; }
+        public string FolderName { get; set; }
+    }
+}
